cb5d0f6b5845be7c651d96ec8c491306a8b77eeb
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-03-01 10:59:12 +0300

    case switch

--- hw-lesson-22/Program.cs
+++ hw-lesson-22/Program.cs
@@ -3,97 +3,108 @@ using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 
-int choise = 4;
+Employee root = null;
 
 while (true)
-
 {
 
-    if (choise == 3)
+    Console.WriteLine("Выберете пункт меню");
+    Console.WriteLine();
+    Console.WriteLine("1 - для ввода данных о сотрудниках, 2 - вывод отсортированного списка");
+    Console.WriteLine("3 - для поиска сотрудника, 4 - для выхода из программы  ");
+
+
+    string choise = Console.ReadLine();
+
+    switch (choise)
+
     {
-        break;
-    }
+        case "1":
 
-
-    bool textCheck = false;
-
-
-
-    Console.WriteLine("введите имя и зарплату сотрудника");
-
-    Employee root = null;
-
-    // ввод имени и зартплаты
-
-    while (true)
-    {
-
-        Console.Write("Введите имя: ");
-        var _name = Console.ReadLine();
-
-        if (string.IsNullOrEmpty(_name))
-        {
-            break;
-        }
-
-
-        Console.Write("Введите зарплату: ");
-
-        var d = int.Parse(Console.ReadLine());
-
-        if (root == null)
-        {
-            root = new Employee()
+            for (int i = 0; i < 1;)
             {
-                Name = _name,
-                Sallary = d
-            };
-        }
-        else
-        {
-            AddEmployee(root, new Employee
-            {
-                Name = _name,
-                Sallary = d
-            });
-        }
-    }
+
+                Console.Write("Введите имя: ");
+                var _name = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(_name))
+                {
+                    break;
+                }
 
 
-    Console.WriteLine("Sorted:");
-    Traverse(root);
+                Console.Write("Введите зарплату: ");
+                Console.WriteLine();
+                var d = int.Parse(Console.ReadLine());
 
-    // поиск по зарплате
+                if (root == null)
+                {
+                    root = new Employee()
+                    {
+                        Name = _name,
+                        Sallary = d
+                    };
+                }
+                else
+                {
+                    AddEmployee(root, new Employee
+                    {
+                        Name = _name,
+                        Sallary = d
+                    });
+                }
 
-    while (true)
-    {
+            }
 
-
-        Console.WriteLine("Какая зарплата ищется");
-
-        int d = int.Parse(Console.ReadLine());
-
-        var (employee, level) = FindEmployee(root, d, level: 1);
-        if (employee == null)
-        {
-            Console.WriteLine("Такой сотрудник не найден");
-        }
-        else
-        {
-            Console.WriteLine($"Сотрудник найден, его имя: {employee.Name}, его зарплата: {employee.Sallary}");
-        }
-
-
-        Console.WriteLine("для нового поиска сотрудника по зарплате введите 1, для ввода нового списка сотрудников введите 0 и 3 для выхода из программы");
-
-        choise = int.Parse(Console.ReadLine());
-
-        if (choise == 0 || choise == 3)
-        {
             break;
-        }
 
+
+        case "2":
+
+            if (root == null)
+            {
+                Console.WriteLine("данных о сотрудниках нет");
+                break;
+            }
+
+            Console.WriteLine("Sorted:");
+            Traverse(root);
+
+            break;
+
+        case "3":
+
+            if (root == null)
+            {
+                Console.WriteLine("данных о сотрудниках нет");
+                break;
+            }
+
+            Console.WriteLine("Какая зарплата ищется");
+
+            int _sallary = int.Parse(Console.ReadLine());
+
+            var (employee, level) = FindEmployee(root, _sallary, level: 1);
+            if (employee == null)
+            {
+                Console.WriteLine("Такой сотрудник не найден");
+            }
+            else
+            {
+                Console.WriteLine($"Сотрудник найден, его имя: {employee.Name}, его зарплата: {employee.Sallary}");
+            }
+
+            break;
+
+        case "4":
+
+            return;
+
+        default:
+            Console.WriteLine("неверный ввод, введите 1, 2, 3 или 4");
+            break;
     }
+
 
 }
 
@@ -151,13 +162,13 @@ static void AddEmployee(Employee root, Employee toAdd)
         //Идем в левое поддерево
 
         if (root.Left != null) AddEmployee(root.Left, toAdd);
-        root.Left = toAdd;
+        else root.Left = toAdd;
     }
     else
     {
         //Идем в правое поддерево
         if (root.Right != null) AddEmployee(root.Right, toAdd);
-        root.Right = toAdd;
+        else root.Right = toAdd;
     }
 }
 
